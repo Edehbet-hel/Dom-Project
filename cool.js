6ffe3fbf942d9537659859b6e567d295ac9f3680
Bethel <bethelededh668@gmail.com>
2024-07-24 16:42:23 -0700

    i added reduce function

--- cool.js
+++ cool.js
@@ -1,18 +1,52 @@
-function getRandomColor(){
-    const letters='0123456789ABCDEF';
-    let color='#';
-    for(let i=0;i < 6; i++){
-        color  +=letters[Math.floor(Math.random()*16)];
-    }
-    return color;
-}
+// function getRandomColor(){
+//     const letters='0123456789ABCDEF';
+//     let color='#';
+//     for(let i=0;i < 6; i++){
+//         color  +=letters[Math.floor(Math.random()*16)];
+//     }
+//     return color;
+// }
 
 
 
-const colorBox=document.getElementById("color-box")
-const btn=document.getElementById("change-color-btn")
+// const colorBox=document.getElementById("color-box")
+// const btn=document.getElementById("change-color-btn")
 
-btn.addEventListener('click', () => {
-    colorBox.style.backgroundColor=getRandomColor();
+// btn.addEventListener('click', () => {
+//     colorBox.style.backgroundColor=getRandomColor();
 
-});
+// });
+
+
+// let name="john"
+// console.log(`Hello ${name},how are you doing?`)
+
+
+
+
+// let names=["Nora","Billy","Rose",];
+// SortedArr=names.sort();
+// console.log(names)
+// names.splice(3 , 0, "Bolt")
+
+// console.log(names);
+
+
+
+// let arr=["2","5","9","1","3","4","7","8"];
+// arr.sort();
+// console.log(arr.reverse())
+// // console.log(sortedArr)
+
+// const colors = ['red', 'blue', 'black','blue']
+// console.log(colors.indexOf('blue',2))
+
+const numbers = [50 / 20]
+
+const reducer = (previousval, newval)=> previousval / newval
+
+console.log(numbers.reduce(reducer),2)
+
+
+
+
